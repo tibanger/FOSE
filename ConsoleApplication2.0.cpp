378221b338f09bb666790916a87849c47bed5ac0
tibanger <in.beleonok@gmail.com>
2023-02-26 16:12:16 +0200

    Update ConsoleApplication2.0.cpp
    replaced "cout << "Hello GitHub!" << endl;" by "cout << 2+2 << endl;"

--- ConsoleApplication2.0.cpp
+++ ConsoleApplication2.0.cpp
@@ -15,7 +15,7 @@ int main()
 	if (a > 2 && b >= 3) cout << "/nСправедлива (true)";
 	if (a < 2 && b <= 3) cout << "Не справедлива (false)";
 	cout << " нерівність A > 2 та B ≥ 3" << endl;
-	cout << "Hello GitHub!" << endl;
+	cout << 2+2 << endl;
 
 	return 0;
 }
